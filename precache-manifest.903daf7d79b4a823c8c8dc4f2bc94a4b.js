cc78bdcdddb972382b7cc1fbaa1a9eb003c72c0f
Obbik <norbertdering@gmail.com>
2020-07-28 15:31:37 +0200

    Updates

---
+++ precache-manifest.903daf7d79b4a823c8c8dc4f2bc94a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231a21ff9a0700b201c60cb18cf81a73",
+    "url": "/snake_react/index.html"
+  },
+  {
+    "revision": "bab43599addce7d0394f",
+    "url": "/snake_react/static/css/main.f552b6b4.chunk.css"
+  },
+  {
+    "revision": "a15700d51d5c26dd402c",
+    "url": "/snake_react/static/js/2.af4d51e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake_react/static/js/2.af4d51e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab43599addce7d0394f",
+    "url": "/snake_react/static/js/main.045ae6b8.chunk.js"
+  },
+  {
+    "revision": "c27a55226360299fe8f8",
+    "url": "/snake_react/static/js/runtime-main.2a85a556.js"
+  }
+]);
